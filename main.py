849e47717c88da528f4d0674b18b638763bac01a
Lisa <you@example.com>
2020-12-06 11:53:21 -0500

    renamed file, added basic saccade

--- main.py
+++ main.py
@@ -4,11 +4,19 @@ Based heavily on "Eye-Hand Behavior in Human-Robot Shared Manipulation" included
 Could theoretically be added to gazebo simulation with project
 """
 import time
-from display import eye_disp
+from eye_model import eye_disp
 
+def saccade(obj, coord1, coord2, duration):
+    t_end = time.time() + duration
+    while time.time() < t_end:
+        obj.look(coord1[0], coord1[1], coord1[2])
+        obj.look(coord2[0], coord2[1], coord2[2])
+
+p1 = (.5, .5, .5)
+p2 = (-.3, .5, 1)
 eyes1 = eye_disp(x_max=700, y_max=400)
-eyes1.look(.5, .5, .5)
-eyes1.look(-.3, .5, 1)
+eyes1.look(p1)
+eyes1.look(p2)
 eyes1.follow_mouse_click(10)
 eyes1.close()
 
